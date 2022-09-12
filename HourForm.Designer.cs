a103d14bcfef327fef12a49627b73cb748c5d384
zacharykollar <77861842+zacharykollar@users.noreply.github.com>
2022-09-11 20:26:00 -0400

    Added: Start/StopWork(), Write to file from app.config

--- HourForm.Designer.cs
+++ HourForm.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UpDownInputHours = new System.Windows.Forms.NumericUpDown();
+            this.textboxHours = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonInputHours = new System.Windows.Forms.Button();
+            this.buttonStartEndWork = new System.Windows.Forms.Button();
+            this.textBoxLoggedHours = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.UpDownInputHours)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hours Worked This Week:";
+            // 
+            // UpDownInputHours
+            // 
+            this.UpDownInputHours.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.UpDownInputHours.Location = new System.Drawing.Point(12, 252);
+            this.UpDownInputHours.Name = "UpDownInputHours";
+            this.UpDownInputHours.Size = new System.Drawing.Size(270, 39);
+            this.UpDownInputHours.TabIndex = 1;
+            // 
+            // textboxHours
+            // 
+            this.textboxHours.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textboxHours.Location = new System.Drawing.Point(12, 46);
+            this.textboxHours.Name = "textboxHours";
+            this.textboxHours.ReadOnly = true;
+            this.textboxHours.Size = new System.Drawing.Size(289, 39);
+            this.textboxHours.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(270, 34);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hours Worked Recently:";
+            // 
+            // buttonInputHours
+            // 
+            this.buttonInputHours.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonInputHours.Location = new System.Drawing.Point(12, 297);
+            this.buttonInputHours.Name = "buttonInputHours";
+            this.buttonInputHours.Size = new System.Drawing.Size(150, 49);
+            this.buttonInputHours.TabIndex = 4;
+            this.buttonInputHours.Text = "Confirm";
+            this.buttonInputHours.UseVisualStyleBackColor = true;
+            // 
+            // buttonStartEndWork
+            // 
+            this.buttonStartEndWork.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonStartEndWork.Location = new System.Drawing.Point(12, 91);
+            this.buttonStartEndWork.Name = "buttonStartEndWork";
+            this.buttonStartEndWork.Size = new System.Drawing.Size(289, 76);
+            this.buttonStartEndWork.TabIndex = 5;
+            this.buttonStartEndWork.Text = "Starting Work?";
+            this.buttonStartEndWork.UseVisualStyleBackColor = true;
+            this.buttonStartEndWork.Click += new System.EventHandler(this.buttonStartEndWork_Click);
+            // 
+            // textBoxLoggedHours
+            // 
+            this.textBoxLoggedHours.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxLoggedHours.Location = new System.Drawing.Point(12, 173);
+            this.textBoxLoggedHours.Name = "textBoxLoggedHours";
+            this.textBoxLoggedHours.ReadOnly = true;
+            this.textBoxLoggedHours.Size = new System.Drawing.Size(289, 39);
+            this.textBoxLoggedHours.TabIndex = 6;
+            // 
+            // HourForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxLoggedHours);
+            this.Controls.Add(this.buttonStartEndWork);
+            this.Controls.Add(this.buttonInputHours);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textboxHours);
+            this.Controls.Add(this.UpDownInputHours);
+            this.Controls.Add(this.label1);
+            this.Name = "HourForm";
             this.Text = "HourForm";
+            ((System.ComponentModel.ISupportInitialize)(this.UpDownInputHours)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private NumericUpDown UpDownInputHours;
+        private TextBox textboxHours;
+        private Label label2;
+        private Button buttonInputHours;
+        private Button buttonStartEndWork;
+        private TextBox textBoxLoggedHours;
     }
 }
--- HourForm.cs
+++ HourForm.cs
@@ -12,20 +12,77 @@ namespace HourLogger
 {
     public partial class HourForm : Form
     {
+        private bool working;
+        private DateTime startTime;
+
+        public bool Working { get => working; set => working = value; }
+        public string OutputFile 
+        { 
+            get => Properties.Settings.Default.OutputFile;
+            
+            set
+            {
+                Properties.Settings.Default.OutputFile = value;
+            }
+        }
+
+        public DateTime StartTime { get => startTime; }
+
         public HourForm()
         {
             InitializeComponent();
+
+            if (OutputFile.Equals(""))
+            {
+                ChangeOutputFile();
+            }
+            Working = false;
         }
 
+
+
+
+        /// <summary>
+        /// Brings up file window to select where to log the file
+        /// </summary>
+        private void ChangeOutputFile()
+        {
+
+        }
         /// <summary>
         /// Writes to the output file, appending to the end
         /// </summary>
         /// <param name="str"></param>
-        public void WriteToFile(int hours)
+        public void WriteToFile(double hours)
         {
-            using( StreamWriter sw = new StreamWriter())
+            using( StreamWriter sw = new StreamWriter(OutputFile))
             {
-                sw.WriteLine(str)
+                sw.WriteLine(hours);
+            }
+        }
+        
+        private void StopWork()
+        {
+            Working = false;
+            double hoursWorked = DateTime.Now.Subtract(StartTime).TotalHours;
+            WriteToFile(hoursWorked);
+        }
+        
+        private void StartWork()
+        {
+            Working = true;
+            startTime = DateTime.Now;
+        }
+
+        private void buttonStartEndWork_Click(object sender, EventArgs e)
+        {
+            if (working)
+            {
+                StopWork();
+            }
+            else
+            {
+                StartWork();
             }
         }
     }
