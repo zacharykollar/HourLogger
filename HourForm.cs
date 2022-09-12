30db7e9867ae896135f5b18e96bb1f2f73101245
zacharykollar <77861842+zacharykollar@users.noreply.github.com>
2022-09-11 20:00:45 -0400

    Add project files.

---
+++ HourForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HourLogger
+{
+    public partial class HourForm : Form
+    {
+        public HourForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Writes to the output file, appending to the end
+        /// </summary>
+        /// <param name="str"></param>
+        public void WriteToFile(int hours)
+        {
+            using( StreamWriter sw = new StreamWriter())
+            {
+                sw.WriteLine(str)
+            }
+        }
+    }
+}
